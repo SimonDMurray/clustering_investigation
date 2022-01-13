a7876439a7a30699e6b72d2d2d480264130b2f66
Simon Murray <simondonaldmurray@gmail.com>
2022-01-13 09:58:09 +0000

    removed original concept code and altered matrix input name to 10x

--- run_r_scripts.sh
+++ run_r_scripts.sh
@@ -30,7 +30,7 @@ do
       cat <<EOU
 -s script to run
 -i path to input file
--r rds input type (matrix input type assumed without this flag)
+-r rds input type (tenx input type assumed without this flag)
 -o path to output location (default CWD)
 -n want neighbour outputs from FindNeighbours function
 -h displays this message!
@@ -73,48 +73,3 @@ mount_options="/lustre,/nfs"
 
 /software/singularity-v3.5.3/bin/singularity exec -B $mount_options $image \
 Rscript $script --input=$input_path --output=$output_path $rds_var $neighbours_var
-
-#if [[ $script_choice == 'c' ]]; then
-#  if [[ $data_type == 'r' ]]; then
-#    if [[ $want_neighbours == 'y' ]]; then
-#      /software/singularity-v3.5.3/bin/singularity exec -B $mount_options $image \
-#      Rscript /lustre/scratch117/cellgen/cellgeni/TIC-misc/tic-1129/actions/simon-seurat.R \
-#      --input=$input_path --output=$output_path -rds -neighbours
-#    elif [[ $want_neighbours == 'n' ]]; then
-#      /software/singularity-v3.5.3/bin/singularity exec -B $mount_options $image \
-#      Rscript /lustre/scratch117/cellgen/cellgeni/TIC-misc/tic-1129/actions/simon-seurat.R \
-#      --input=$input_path --output=$output_path -rds
-#    fi
-#  elif [[ $data_type == 'm' ]]; then
-#    if [[ $want_neighbours == 'y' ]]; then
-#      /software/singularity-v3.5.3/bin/singularity exec -B $mount_options $image \
-#      Rscript /lustre/scratch117/cellgen/cellgeni/TIC-misc/tic-1129/actions/simon-seurat.R \
-#      --input=$input_path --output=$output_path -neighbours
-#    elif [[ $want_neighbours == 'n' ]]; then
-#      /software/singularity-v3.5.3/bin/singularity exec -B $mount_options $image \
-#      Rscript /lustre/scratch117/cellgen/cellgeni/TIC-misc/tic-1129/actions/simon-seurat.R \
-#      --input=$input_path --output=$output_path
-#    fi
-#  fi
-#elif [[ $script_choice == 'b' ]]; then
-#  if [[ $data_type == 'r' ]]; then
-#      /software/singularity-v3.5.3/bin/singularity exec -B $mount_options $image \
-#      Rscript /lustre/scratch117/cellgen/cellgeni/TIC-misc/tic-1129/actions/biology-info.R  \
-#      --input=$input_path --output=$output_path -rds
-#  elif [[ $data_type == 'm' ]]; then
-#    /software/singularity-v3.5.3/bin/singularity exec -B $mount_options $image \
-#    Rscript /lustre/scratch117/cellgen/cellgeni/TIC-misc/tic-1129/actions/biology-info.R  \
-#    --input=$input_path --output=$output_path
-#  fi
-#elif [[ $script_choice == 'p' ]]; then
-#  if [[ $data_type == 'r' ]]; then
-#    /software/singularity-v3.5.3/bin/singularity exec -B $mount_options $image \
-#    Rscript /lustre/scratch117/cellgen/cellgeni/TIC-misc/tic-1129/actions/pca_statistics.R  \
-#    --input=$input_path --output=$output_path -rds
-#  elif [[ $data_type == 'm' ]]; then
-#    /software/singularity-v3.5.3/bin/singularity exec -B $mount_options $image \
-#    Rscript /lustre/scratch117/cellgen/cellgeni/TIC-misc/tic-1129/actions/pca_statistics.R  \
-#    --input=$input_path --output=$output_path
-#  fi
-#fi
-
